09520e49969b00ef4f48cab73aaff4d0a4ef54c7
hisyam maulana <icham95@gmail.com>
2017-08-04 13:02:25 +0700

    add init x-template register, login, home

--- index.php
+++ index.php
@@ -8,14 +8,74 @@
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+<div id="app">
   <header>
-  
+    <div>
+      <router-link to="/"> Votex </router-link>
+    </div>
+    <div>
+      <router-link to="login"> Login </router-link>
+      <router-link to="register"> Daftar </router-link>
+    </div>
   </header>
 
+  <button @click="">
+    test
+  </button>
+
+  <router-view></router-view>
+
+</div>
+
+  <script type="text/x-template" id="t-login">
+    <div>
+      login
+    </div>
+  </script>
+
+  <script type="text/x-template" id="t-home">
+    <div>
+      home
+    </div>
+  </script>
+
+  <script type="text/x-template" id="t-register">
+    <div>
+      register
+    </div>
+  </script>
+
   <script src="https://unpkg.com/vue"></script>
+  <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
   <script src="https://unpkg.com/vuex"></script>
+
   <script>
     
+    const Login = {
+      template: '#t-login'
+    }
+
+    const Home = {
+      template: '#t-home'
+    }
+
+    const Register = {
+      template: '#t-register'
+    }
+
+    const routes = [
+      { path: '/', component: Home },
+      { path: '/login', component: Login },
+      { path: '/register', component: Register }
+    ]
+    
+    const router = new VueRouter({
+      routes
+    })
+
+    const app = new Vue ({
+      router
+    }).$mount('#app')
   </script>
 </body>
 </html>
